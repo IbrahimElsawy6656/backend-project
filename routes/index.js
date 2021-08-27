66106b10ede2a755530c8126474f951bee797432
Ibrahim Gamal <48655736+IbrahimElsawy6656@users.noreply.github.com>
2021-08-27 12:51:27 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send("Home");
+      res.render('index', { term: `NONE`, results: [] });
 });
 
 module.exports = router;
